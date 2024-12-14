c8fa34beaff69ac3a2be6ab013da834637d6ce52
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-12-14 15:38:50 -0700

    Update day6.nim

--- nim/day6.nim
+++ nim/day6.nim
@@ -1,12 +1,13 @@
 import sequtils, strformat, tools, tables, sets
 
 type 
+
     SSC = seq[seq[char]]
-    SI = seq[int]
-    TSI = Table[SI, int]
-    HSI = HashSet[SI]
+    TI = tuple[x, y: int]
+    TSI = Table[TI, int]
+    HSI = HashSet[TI]
     
-proc readInput(): (SSC, SI, TSI) =
+proc readInput(): (SSC, TI, TSI) =
     let file = open("input/day6.txt");
     defer: file.close()
 
@@ -16,24 +17,24 @@ proc readInput(): (SSC, SI, TSI) =
     for r_ind, r_val in result[0]:
       for c_ind, c_val in r_val:
         if c_val == '^':
-          result[1] = @[r_ind, c_ind]        
+          result[1] = (r_ind, c_ind)
         if c_val == '#':
-          result[2][@[r_ind, c_ind]] = 0
+          result[2][(r_ind, c_ind)] = 0
           
 var (mat, start, locs) = readInput()
 let
   width = mat[0].len() - 1
   height = mat.len() - 1 
-  dirs = {0: @[-1, 0], 1: @[0, 1], 2: @[1, 0], 3: @[0, -1]}.toTable
+  dirs = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}.toTable
   
-proc pt1(mat: SSC, start: SI): (int, HSI) =
+proc pt1(mat: SSC, start: TI): (int, HSI) =
   var 
     facing = 0
     pos = start
   
   while (pos[0] in 1..<height) and (pos[1] in 1..<width):
     let next_pos = pos + dirs[facing] 
-    if arrValue(mat, next_pos) == '#':
+    if fetchVal(mat, next_pos) == '#':
       inc facing
       if facing == 4:
         facing = 0
@@ -44,21 +45,21 @@ proc pt1(mat: SSC, start: SI): (int, HSI) =
   result[1].incl(pos)
   result[0] = result[1].len()
 
-proc pt2(mat: var SSC, start: SI, empty: HSI, locs: var TSI): int =
+proc pt2(mat: var SSC, start: TI, empty: HSI, locs: var TSI): int =
   for i in empty:
     var
       facing = 0
       pos = start
-    mat[i[0]][i[1]] = '#'
+    mat[i.x][i.y] = '#'
     locs[i] = 0
       
     while (pos[0] in 1..<height) and (pos[1] in 1..<width):
       let next_pos = pos + dirs[facing]
-      if arrValue(mat, next_pos) == '#':
+      if fetchVal(mat, next_pos) == '#':
         inc facing 
         inc locs[next_pos]
         if locs[next_pos] == 5:
-          echo &"Obstacle was at pos: [{i[0]},{i[1]}]" 
+          echo &"Obstacle was at pos: [{i.tupToStr()}]" 
           inc result
           break
         if facing == 4:
@@ -66,7 +67,7 @@ proc pt2(mat: var SSC, start: SI, empty: HSI, locs: var TSI): int =
       else:
         pos = next_pos
         
-    mat[i[0]][i[1]] = '.'
+    mat[i.x][i.y] = '.'
     for key in locs.keys:
       locs[key] = 0
     locs.del(i)
