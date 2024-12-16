94a4596f371a2203a65b400755e5debc1a8201b2
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-12-15 17:50:02 -0700

    Add files via upload

---
+++ v/day7.v
@@ -0,0 +1,63 @@
+import os
+
+fn main() {
+	targets, nums := parse_file()
+	pt1, pt2 := solution(targets, nums)
+	println('Part 1: ${pt1}\nPart 2: ${pt2}')
+}
+
+fn parse_file() ([]i64, [][]i64) {
+	lines := os.read_lines('input/day7.txt') or { panic(err) }
+	mut targets := []i64{}
+	mut nums := [][]i64{} 
+
+	for i in lines {
+		tmp := i.replace(" ", ",").split(":,")
+		targets << tmp[0].i64()
+		nums << tmp[1].split(",").map(it.i64())
+	}
+
+	return targets, nums
+}
+
+fn concat(a i64, b i64) i64 {
+	return "${a}${b}".i64()
+}
+
+fn evaluate(nums []i64, target i64, index i64, currentVal i64, pt1 bool) bool {
+	if index == 0 {
+		return evaluate(nums, target, index + 1, nums[0], pt1)
+	}
+
+	if index == nums.len {
+		return currentVal == target
+	}
+
+	num := nums[index]
+
+	if evaluate(nums, target, index + 1, currentVal + num, pt1) {
+		return true
+	}
+	if evaluate(nums, target, index + 1, currentVal * num, pt1) {
+		return true
+	}
+	if !pt1 && evaluate(nums, target, index + 1, concat(currentVal, num), pt1) {
+		return true
+	}
+	return false
+}
+
+fn solution(a []i64, b [][]i64) (i64, i64) {
+	mut ttl := i64(0)
+	mut ttl2 := i64(0)
+
+	for ind, vals in b {
+		if evaluate(vals, a[ind], 0, 0, true) {
+			ttl += a[ind]
+		}
+		if evaluate(vals, a[ind], 0, 0, false) {
+			ttl2 += a[ind]
+		}
+	}
+	return ttl, ttl2
+}
