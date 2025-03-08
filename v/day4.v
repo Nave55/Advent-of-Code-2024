43fedf1d4d454361a10d4866a4af8c4eb6b2d1c2
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-03-08 11:00:57 -0700

    Update day4.v

--- v/day4.v
+++ v/day4.v
@@ -1,11 +1,10 @@
 import os
-import tools {Vec2, nbrs, arr_value, in_bounds}
+import tools { Vec2, arr_value, in_bounds, nbrs }
 
 fn main() {
 	pt1, pt2 := solution()!
 	println('Part 1: ${pt1}\nPart 2: ${pt2}')
 }
-
 
 fn solution() !(int, int) {
 	lines := os.read_lines('input/day4.txt')!
@@ -18,28 +17,48 @@ fn solution() !(int, int) {
 	return check_xmas(arr), check_x(arr)
 }
 
-fn check_xmas(mat [][]rune) u32 {
+fn check_xmas(mat [][]rune) int {
 	width := mat[0].len
 	height := mat.len
 
 	dirs := [
-		Vec2[int]{-3, -3}, Vec2[int]{-2, -2}, Vec2[int]{-1, -1}, Vec2[int]{-3, 3}, Vec2[int]{-2, 2}, Vec2[int]{-1, 1},
-		Vec2[int]{3, -3}, Vec2[int]{2, -2}, Vec2[int]{1, -1}, Vec2[int]{3, 3}, Vec2[int]{2, 2}, Vec2[int]{1, 1},
-		Vec2[int]{-3, 0}, Vec2[int]{-2, 0}, Vec2[int]{-1, 0}, Vec2[int]{3, 0}, Vec2[int]{2, 0}, Vec2[int]{1, 0},
-		Vec2[int]{0, -3}, Vec2[int]{0, -2}, Vec2[int]{0, -1}, Vec2[int]{0, 3}, Vec2[int]{0, 2}, Vec2[int]{0, 1}
-		]
+		Vec2[int]{-3, -3},
+		Vec2[int]{-2, -2},
+		Vec2[int]{-1, -1},
+		Vec2[int]{-3, 3},
+		Vec2[int]{-2, 2},
+		Vec2[int]{-1, 1},
+		Vec2[int]{3, -3},
+		Vec2[int]{2, -2},
+		Vec2[int]{1, -1},
+		Vec2[int]{3, 3},
+		Vec2[int]{2, 2},
+		Vec2[int]{1, 1},
+		Vec2[int]{-3, 0},
+		Vec2[int]{-2, 0},
+		Vec2[int]{-1, 0},
+		Vec2[int]{3, 0},
+		Vec2[int]{2, 0},
+		Vec2[int]{1, 0},
+		Vec2[int]{0, -3},
+		Vec2[int]{0, -2},
+		Vec2[int]{0, -1},
+		Vec2[int]{0, 3},
+		Vec2[int]{0, 2},
+		Vec2[int]{0, 1},
+	]
 
 	mut valid_inds := []Vec2[int]{}
 	for r_ind, r_val in mat {
 		for c_ind, c_val in r_val {
-			tmp := Vec2{r_ind, c_ind}
 			if c_val == `X` {
+				tmp := Vec2{r_ind, c_ind}
 				valid_inds << tmp
 			}
 		}
 	}
 
-	mut ttl := u32(0)
+	mut ttl := int(0)
 	mut cnt := 0
 	for v in valid_inds {
 		mut str := []rune{}
@@ -52,18 +71,18 @@ fn check_xmas(mat [][]rune) u32 {
 			if cnt % 3 == 0 {
 				str << `X`
 				s := str.string()
-				if s == "XMAS"|| s == "SAMX" {
+				if s == 'XMAS' || s == 'SAMX' {
 					ttl += 1
 				}
 				str.clear()
 			}
 		}
-	}	
+	}
 	return ttl
-} 
+}
 
-fn check_x(mat [][]rune) u32 {
-	mut ttl := u32(0)
+fn check_x(mat [][]rune) int {
+	mut ttl := int(0)
 	mut valid_inds := []Vec2[int]{}
 	for r_ind, r_val in mat {
 		for c_ind, c_val in r_val {
@@ -76,7 +95,7 @@ fn check_x(mat [][]rune) u32 {
 	for i in valid_inds {
 		_, vals := nbrs(mat, i, `d`)
 		tmp := vals.string()
-		if tmp in ["MMSS", "SSMM", "SMSM", "MSMS"] {
+		if tmp in ['MMSS', 'SSMM', 'SMSM', 'MSMS'] {
 			ttl++
 		}
 	}
