081267cf7aa5cecc9fa4b3962fd3e9fc11977fc1
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-03-09 14:15:56 -0600

    Update Day6.hx

--- haxe/Day6.hx
+++ haxe/Day6.hx
@@ -6,78 +6,80 @@ using hx.strings.Strings;
 using Lambda;
 
 class Day6 {
-    static var dirs = [0 => [-1, 0], 1 => [0, 1], 2 => [1, 0], 3 => [0, -1]];
+    static var dirs: Map<Int, Vec2> = [0 => {x: -1, y: 0}, 1 => {x: 0, y: 1}, 2 => {x: 1, y: 0}, 3 => {x: 0, y: -1}];
+    static var rows = 130;
+    static var cols = 130;
     static function main() {
         var con = parse_file();
         var s1 = solution(con.mat, con.start);
-        var s2 = solution2(con.mat, con.start, con.locs, s1.visited);
-        Sys.println('Part 1: ${s1.ttl}\nPart 2: ${s2}');
+        var s2 = solution2(con.mat, con.start, con.locs, s1);
+        Sys.println('Part 1: ${s1.count()}\nPart 2: ${s2}');
     }
 
     static function parse_file() {
         var con = [for (i in sys.io.File.getContent('input/Day6.txt').split("\r\n")) i.split("")];
-        var start: AI = [];
+        var start: Vec2 = {x: 0, y: 0};
         var locs: Map<String, Int> = [];  
 
         for (r_ind => r_val in con) {
             for (c_ind => c_val in r_val) {
-                if (c_val == "^") start = [r_ind, c_ind];
-                if (c_val == "#") locs['${r_ind},${c_ind}'] = 0;
+                if (c_val == "^") start = {x: r_ind, y: c_ind};
+                if (c_val == "#") locs[vecToStr({x: r_ind, y: c_ind})] = 0;
             }
         }
         return {mat: con, start: start, locs: locs}
     }
 
-    static function solution(mat: AAS, start: AI) {
+    static function inbounds(pos: Vec2) {
+        return (pos.x > 0 && pos.x < rows - 1) && (pos.y > 0 && pos.y < cols - 1);
+    }
+
+    static function solution(mat: AAS, start: Vec2) {
         var facing = 0;
         var pos = start;
-        var visited: Set<String> = new Set([]);
-        while ((pos[0] > 0 && pos[0] < mat.length -1) && pos[1] > 0 && pos[1] < mat[0].length - 1) {
-            var v_pos = new Vec(pos);
+        var visited: Map<String, {}> = [];
+        while (inbounds(pos)) {
+            var v_pos = new Tup(pos);
             var next_pos = v_pos + dirs[facing];
-            if (arrValue(mat, next_pos) == '#') {
+            if (mat[next_pos.x][next_pos.y] == "#") {
                 facing++;
-                if (facing == 4) facing = 0;
-            }
-            else {
-                visited.push('${pos[0]},${pos[1]}');
+                facing = facing % 4;
+            } else {
+                visited[vecToStr({x: pos.x, y: pos.y})] = {};
                 pos = next_pos;
             }
         }
-        visited.push('${pos[0]},${pos[1]}');
+        visited[vecToStr({x: pos.x, y: pos.y})] = {};
 
-        return {ttl: visited.rtrnArray().length, visited: visited};
+        return visited;
     }
 
-
-    static function solution2(mat: AAS, start: AI, locs: Map<String, Int>, empty: Set<String>) {
+    static function solution2(mat: AAS, start: Vec2, locs: Map<String, Int>, empty: Map<String, {}>) {
         var ttl = 0;
-        for (i_val in empty.rtrnArray()) {
-            var i = i_val.split(',').map(item -> parseInt(item) ?? 0);
+        for (i in empty.keys()) {
+            var i_vec = strToVec(i);
             var facing = 0;
             var pos = start;
-            mat[i[0]][i[1]] = "#";
-            locs[i_val] = 0;
+            mat[i_vec.x][i_vec.y] = "#";
+            locs[i] = 0;
 
-            while ((pos[0] > 0 && pos[0] < mat.length -1) && pos[1] > 0 && pos[1] < mat[0].length - 1) {
-                var v_pos = new Vec(pos);
+            while (inbounds(pos)) {
+                var v_pos = new Tup(pos);
                 var next_pos = v_pos + dirs[facing];
-                var n_pos_s = '${next_pos[0]},${next_pos[1]}';
-                if (arrValue(mat, next_pos) == "#") {
+                if (mat[next_pos.x][next_pos.y] == "#") {
                     facing++;
-                    locs[n_pos_s]++;
-                    if (locs[n_pos_s] == 4) {
-                        trace('Obstacle was at pos ${i[0]},${i[1]}');
+                    locs[vecToStr(next_pos)]++;
+                    if (locs[vecToStr(next_pos)] == 4) {
+                        // trace('Obstacle was at pos ${i[0]},${i[1]}');
                         ttl++;
                         break;
                     }
-                    if (facing == 4) facing = 0;
-                }
-                else pos = next_pos;
+                    facing = facing % 4;
+                } else pos = next_pos;
             }
-            mat[i[0]][i[1]] = '.';
+            mat[i_vec.x][i_vec.y] = '.';
             for (key in locs.keys()) locs[key] = 0;
-            locs.remove(i_val);
+            locs.remove(i);
         }
 
         return ttl;
