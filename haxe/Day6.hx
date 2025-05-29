5f9d48ed814042b50422bb0b415d3266da65fe48
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-05-28 21:41:44 -0600

    Update Day6.hx

--- haxe/Day6.hx
+++ haxe/Day6.hx
@@ -1,9 +1,13 @@
 import StringTools.*;
 import Std.*;
 import Tools;
+import haxe.ds.Vector;
+import haxe.ds.StringMap;
 
 using hx.strings.Strings;
 using Lambda;
+
+typedef VVS = Vector<Vector<String>>;
 
 class Day6 {
     static var dirs: Array<Vec2> = [{x: -1, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}, {x: 0, y: -1}];
@@ -18,45 +22,55 @@ class Day6 {
     }
 
     static function parse_file() {
-        var con = [for (i in sys.io.File.getContent('input/Day6.txt').split("\r\n")) i.split("")];
+        var con: Vector<Vector<String>> = new Vector(rows);
+        for (r_ind => r_val in sys.io.File.getContent('input/Day6.txt').split("\r\n")) {
+            con[r_ind] = new Vector(cols);
+            for (c_ind in 0...cols) {
+                con[r_ind][c_ind] = r_val.charAt(c_ind);
+            } 
+        }
+
         var start: Vec2 = {x: 0, y: 0};
         var locs: Map<String, Int> = [];  
 
-        for (r_ind => r_val in con) {
-            for (c_ind => c_val in r_val) {
-                if (c_val == "^") start = {x: r_ind, y: c_ind};
-                if (c_val == "#") locs[vecToStr({x: r_ind, y: c_ind})] = 0;
+        for (r_ind in 0...rows) {
+            for (c_ind in 0...cols) {
+                var val = {x: r_ind, y: c_ind};
+                if (con[r_ind][c_ind] == "^") start = val;
+                if (con[r_ind][c_ind] == "#") locs[vecToStr(val)] = 0;
             }
         }
         return {mat: con, start: start, locs: locs}
     }
 
-    static function inbounds(pos: Vec2) {
+    static inline function inbounds(pos: Vec2) {
         return (pos.x > 0 && pos.x < rows - 1) && (pos.y > 0 && pos.y < cols - 1);
     }
 
-    static function solution(mat: AAS, start: Vec2) {
+    static function solution(mat: VVS, start: Vec2) {
         var facing = 0;
         var pos = start;
-        var visited: Map<String, {}> = [];
+        var visited: StringMap<Bool> = new StringMap();
+
         while (inbounds(pos)) {
             var v_pos = new Tup(pos);
             var next_pos = v_pos + dirs[facing];
             if (mat[next_pos.x][next_pos.y] == "#") {
                 facing++;
-                facing = facing % 4;
+                facing = facing & 3;
             } else {
-                visited[vecToStr({x: pos.x, y: pos.y})] = {};
+                visited.set(vecToStr({x: pos.x, y: pos.y}), true);
                 pos = next_pos;
             }
         }
-        visited[vecToStr({x: pos.x, y: pos.y})] = {};
+        visited.set(vecToStr({x: pos.x, y: pos.y}), true);
 
         return visited;
     }
 
-    static function solution2(mat: AAS, start: Vec2, locs: Map<String, Int>, empty: Map<String, {}>) {
+    static function solution2(mat: VVS, start: Vec2, locs: Map<String, Int>, empty: StringMap<Bool>) {
         var ttl = 0;
+
         for (i in empty.keys()) {
             var i_vec = strToVec(i);
             var facing = 0;
@@ -65,7 +79,7 @@ class Day6 {
             locs[i] = 0;
 
             while (inbounds(pos)) {
-                var v_pos = new Tup(pos);
+                var v_pos = new Tup(pos); 
                 var next_pos = v_pos + dirs[facing];
                 if (mat[next_pos.x][next_pos.y] == "#") {
                     facing++;
@@ -75,7 +89,7 @@ class Day6 {
                         ttl++;
                         break;
                     }
-                    facing = facing % 4;
+                    facing = facing & 3;
                 } else pos = next_pos;
             }
             mat[i_vec.x][i_vec.y] = '.';
