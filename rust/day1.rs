d53108ce63581ee325624c2d7739c7360f8b2309
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-08-04 18:47:39 -0600

    Update day1.rs

--- rust/day1.rs
+++ rust/day1.rs
@@ -1,25 +1,32 @@
 use std::{collections::HashSet, fs};
 
 fn main() {
-    let (left, right) = parse_file().expect("Failed to parse file");
+    let (left, right) = parse_file();
     let pt1 = solution(&left, &right);
     let pt2 = solution2(&left, &right);
     println!("Part 1: {}\nPart 2: {}", pt1, pt2);
 }
 
-fn parse_file() -> Result<(Vec<i32>, Vec<i32>), Box<dyn std::error::Error>> {
-    let mut left: Vec<i32> = Vec::new();
-    let mut right: Vec<i32> = Vec::new();
-    let con = fs::read_to_string("../inputs/day1.txt")?;
-    con.split("\r\n\r\n").flat_map(|n| n.lines()).for_each(|l| {
-        let tmp: Vec<&str> = l.split("   ").collect();
-        left.push(tmp[0].parse::<i32>().expect("Failed to parse left value"));
-        right.push(tmp[1].parse::<i32>().expect("Failed to parse right value"));
-    });
+fn parse_file() -> (Vec<i32>, Vec<i32>) {
+    let con = fs::read_to_string("../inputs/day1.txt").expect("Failed to read file");
+
+    let pairs: Vec<(i32, i32)> = con
+        .lines()
+        .filter_map(|line| {
+            let mut nums = line
+                .split_whitespace()
+                .filter_map(|s| s.parse::<i32>().ok());
+            Some((nums.next()?, nums.next()?))
+        })
+        .collect();
+
+    let mut left: Vec<i32> = pairs.iter().map(|(left, _)| *left).collect();
+    let mut right: Vec<i32> = pairs.iter().map(|(_, right)| *right).collect();
 
     left.sort_unstable();
     right.sort_unstable();
-    Ok((left, right))
+
+    (left, right)
 }
 
 fn solution(left: &[i32], right: &[i32]) -> i32 {
