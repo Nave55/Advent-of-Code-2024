838f9e49f4b37fe874593aba4c213a64802f2203
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-09-29 01:15:14 -0600

    Update day2.nim

--- nim/day2.nim
+++ nim/day2.nim
@@ -1,47 +1,54 @@
 import strutils, strformat, sequtils, sugar
 
-type
-    IntSeq = seq[int]
+type IntSeq = seq[int]
 
 proc readInput(): seq[IntSeq] =
-    let file = open("input/day2.txt")
-    defer: file.close()
+  let file = open("input/day2.txt")
+  defer: file.close()
 
-    for i in file.lines():
-         result &= i.splitWhitespace().map(item => parseInt(item))
+  for i in file.lines():
+    result &= i.splitWhitespace().map(item => parseInt(item))
          
-let con = readInput()
+func checkSafety(arr: IntSeq): bool =
+  if arr.len <= 1:
+    return true
 
-func solver(arr: IntSeq, n: int, pt1: bool): int =
-    if n == arr.len(): return 0
+  var
+    is_inc_safe = true
+    is_dec_safe = true
 
+  for i in 0..<arr.len - 1:
+    if not is_dec_safe and not is_inc_safe:
+      break
+    let val = arr[i] - arr[i + 1]
+    if is_inc_safe:
+      is_inc_safe = val >= 1 and val <= 3
+    if is_dec_safe:
+      is_dec_safe = val <= -1 and val >= -3
+
+  return is_inc_safe or is_dec_safe
+
+func checkSafetyTwo(arr: IntSeq): bool =
+  if checkSafety(arr):
+    return true
+
+  for ind, _ in arr:
     var tmp = arr
-    if not pt1: tmp.delete(n)
+    tmp.delete(ind)
+    if checkSafety(tmp):
+      return true
 
-    let p = if tmp.len() > 1: tmp[0] - tmp[1] else: 0
 
-    for j in 1..<tmp.len():
-        let dist = tmp[j - 1] - tmp[j]
-        if p >= 0:
-            if dist > 3 or dist <= 0:
-                if pt1: return 0
-                break
-            if j == tmp.len() - 1:
-                return 1
-        elif p <= 0:
-            if dist < -3 or dist >= 0:
-                if pt1: return 0
-                break
-            if j == tmp.len() - 1:
-                return 1
-
-    return solver(arr, n + 1, false)
+  return false
 
 proc solution(): (int, int) =    
-    for i in con:
-        result[0] += solver(i, 0, true)
-        result[1] += solver(i, 0, false)
+  let con = readInput()
+
+  for i in con:
+    if checkSafety(i):
+      inc result[0]
+    if checkSafetyTwo(i):
+      inc result[1]
 
 let (pt1, pt2) = solution()
-
 echo &"Part 1: {pt1}\nPart 2: {pt2}"
