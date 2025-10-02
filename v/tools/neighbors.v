8c070d57c936e223dfc580b7e6cf4f4fbb10ebde
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-10-02 17:12:38 -0600

    Update neighbors.v

--- v/tools/neighbors.v
+++ v/tools/neighbors.v
@@ -1,5 +1,11 @@
 module tools
 
+pub enum Dirs {
+	all
+	diags
+	udlr
+}
+
 pub fn add_arrs[T](arr []T, arr2 []T) []T {
 	mut n := []T{}
 	for i in 0..arr.len {
@@ -17,17 +23,21 @@ pub fn in_bounds(pos Vec2[int], height int, width int) bool {
 }
 
 
-pub fn nbrs[T](arr [][]T, loc Vec2[int], t rune) ([]Vec2[int], []T) {
+pub fn nbrs[T](arr [][]T, loc Vec2[int], t Dirs) ([]Vec2[int], []T) {
 	mut dir := []Vec2[int]{}
-    if t == `n` {
-		dir = [Vec2[int]{-1, 0}, Vec2[int]{0, -1}, Vec2[int]{0, 1}, Vec2[int]{1, 0}]
+	match t {
+		.udlr {
+			dir = [Vec2[int]{-1, 0}, Vec2[int]{1, 0}, Vec2[int]{0, 1}, Vec2[int]{0, -1}]
+		}
+		.diags {
+			dir = [Vec2[int]{-1, -1}, Vec2[int]{-1, 1}, Vec2[int]{1, -1}, Vec2[int]{1, 1}]
+		}
+		.all {
+			dir = [Vec2[int]{-1, -1}, Vec2[int]{-1, 0}, Vec2[int]{-1, 1}, Vec2[int]{0, -1}, Vec2[int]{0, 1}, Vec2[int]{1, -1}, Vec2[int]{1, 0}, Vec2[int]{1, 1}]
+
+		}
 	}
-	else if t == `d` {
-		dir = [Vec2[int]{-1, -1}, Vec2[int]{-1, 1}, Vec2[int]{1, -1}, Vec2[int]{1, 1}]
-	} 
-	else if t== `b`  {
-		dir = [Vec2[int]{-1, -1}, Vec2[int]{-1, 0}, Vec2[int]{-1, 1}, Vec2[int]{0, -1}, Vec2[int]{0, 1}, Vec2[int]{1, -1}, Vec2[int]{1, 0}, Vec2[int]{1, 1}]
-	}
+
     mut indices := []Vec2[int]{}
     mut vals := []T{}
     for i in dir {
