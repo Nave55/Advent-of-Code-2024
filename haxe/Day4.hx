8c3e701d95f14a1e42c5460af5dea3f2045c0eeb
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-10-02 15:45:41 -0600

    Update Day4.hx

--- haxe/Day4.hx
+++ haxe/Day4.hx
@@ -3,9 +3,14 @@ import Tools;
 using hx.strings.Strings;
 using Lambda;
 
-
-
 class Day4 {
+    static final dirs: AV2 = [
+        {x: -3,  y: -3}, {x: -2, y: -2}, {x: -1, y: -1}, {x: -3, y: 3}, {x: -2, y: 2}, {x: -1, y: 1}, 
+        {x: 3, y: -3}, {x: 2, y: -2}, {x: 1, y: -1}, {x: 3, y: 3}, {x: 2, y: 2}, {x: 1, y: 1},
+        {x: -3, y: 0}, {x: -2, y: 0}, {x: -1, y: 0}, {x: 3, y: 0}, {x: 2, y: 0}, {x: 1, y: 0},
+        {x: 0, y: -3}, {x: 0, y: -2}, {x: 0, y: -1}, {x: 0, y: 3}, {x: 0, y: 2}, {x: 0, y: 1}
+    ];
+
     static function main() {
         var con = parse_file();
         var pt1 = checkXmas(con);
@@ -18,54 +23,40 @@ class Day4 {
     }
 
     static function checkXmas(mat: AAC) {
-        var width = mat[0].length, height = mat.length;
-        var dirs: AAI = [
-            [-3, -3], [-2, -2], [-1, -1], [-3, 3], [-2, 2], [-1, 1], 
-            [3, -3], [2, -2], [1, -1], [3, 3], [2, 2], [1, 1],
-            [-3, 0], [-2, 0], [-1, 0], [3, 0], [2, 0], [1, 0],
-            [0, -3], [0, -2], [0, -1], [0, 3], [0, 2], [0, 1]
-        ];
-    
-        var valid_inds: AAI = [];
+        final width = mat[0].length, height = mat.length;
+        var ttl = 0;
+
         for (r_ind => r_val in mat) {
             for (c_ind => c_val in r_val) {
-                var tmp = [r_ind, c_ind];
-                if (c_val == 'X') valid_inds.push(tmp);
+                if (c_val != 'X') continue;
+                var str = "";
+                var cnt = 0; 
+                for (val in dirs) {
+                    cnt++;
+                    var vec = new Tup({x: r_ind, y: c_ind});
+                    var loc = vec + val;
+                    if (inBounds(loc, width, height)) str += fetchVal(mat, loc);
+                    if (cnt % 3 == 0) {
+                        str += "X";
+                        if (str == "XMAS" || str == "SAMX") ttl++; 
+                        str = "";
+                    }
+                }
             }
         }
         
-        var ttl = 0, cnt = 0; 
-        for (v in valid_inds) {
-            var str = "";
-            for (val in dirs) {
-                cnt++;
-                var vec = new Vec(v);
-                var loc = vec + [val[0], val[1]];
-                if  ((loc[0] >= 0 && loc[0] < height) && (loc[1] >= 0 && loc[1] < width)) {
-                    str += mat[loc[0]][loc[1]];
-                }
-                if (cnt % 3 == 0) {
-                    str += "X";
-                    if (str == "XMAS" || str == "SAMX") ttl++; 
-                    str = "";
-                }
-            }
-        }
         return ttl;
     }  
 
     static function checkX(mat: AAC) {
         var ttl = 0;
-        var valid_inds: AAI = [];
         for (r_ind => r_val in mat) {
             for (c_ind => c_val in r_val) {
-                if (c_val == 'A') valid_inds.push([r_ind, c_ind]);
+                if (c_val != 'A') continue;
+                var vals = nbrs(mat, {x: r_ind, y: c_ind}, "diag").vals.map(item -> item.toString()).join("");
+                if (["MMSS", "SSMM", "SMSM", "MSMS"].contains(vals)) ttl++;
             }
         }
-        for (i in valid_inds) {
-            var vals = Tools.nbrs(mat, [i[0], i[1]], "diag").vals.map(item -> item.toString()).join("");
-            if (["MMSS", "SSMM", "SMSM", "MSMS"].contains(vals)) ttl++;
-        }
-	return ttl;
+    	return ttl;
     }
 }
