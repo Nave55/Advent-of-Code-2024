c2c0ea751b9c5422600126584840723376aadefa
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-10-12 12:25:48 -0600

    Update day2.cpp

--- cpp/day2.cpp
+++ cpp/day2.cpp
@@ -1,54 +1,54 @@
+#include <print>
 #include "tools.h"
-#include <tl/getlines.hpp>
-#include <tl/to.hpp>
-#include <fstream>
-#include <ranges>
 
-auto checkSafety(const vi &arr) -> bool {
-    if (arr.size() <= 1) return true;
+auto checkSafety(std::span<int> arr) -> bool {
+  if (arr.size() <= 1) return true;
 
-    bool is_inc = true, is_dec = true;
-    for (size_t i = 0; i < arr.size() - 1; i++) {
-        if (!is_inc && !is_dec) return false;
-        int val = arr[i] - arr[i + 1];
-        if (is_inc) is_inc = val >= 1 && val <= 3;
-        if (is_dec) is_dec = val <= -1 && val >= -3;
-    }
+  bool is_inc = true, is_dec = true;
+  for (size_t i = 0; i < arr.size() - 1; i++) {
+    if (!is_inc && !is_dec) return false;
+    int val = arr[i] - arr[i + 1];
+    if (is_inc) is_inc = val >= 1 && val <= 3;
+    if (is_dec) is_dec = val <= -1 && val >= -3;
+  }
 
-    return is_inc || is_dec;
+  return is_inc || is_dec;
 }
 
-auto checkSafetyTwo(const vi &arr) -> bool {
-    if (checkSafety(arr)) return true;
+auto checkSafetyTwo(std::span<int> arr) -> bool {
+  if (checkSafety(arr)) return true;
 
-    for (size_t i = 0; i < arr.size(); i++) {
-        auto tmp = arr;
-        tmp.erase(tmp.begin() + i);
-        if (checkSafety(tmp)) return true;
-    }
+  for (size_t i = 0; i < arr.size(); i++) {
+    vi tmp(arr.begin(), arr.end());
+    tmp.erase(tmp.begin() + i);
+    if (checkSafety(tmp)) return true;
+  }
 
-    return false;
+  return false;
 }
 
-auto solution() -> void {
-    std::ifstream file ("input/day2.txt");
-    auto lines = tl::views::getlines(file) | tl::to<std::vector<std::string>>();
-    int sum1 {0}, sum2 {0};
+auto solution(const char* path) -> void {
+  std::ifstream file(path);
+  assert(file);
+  std::string line;
 
-    for (const auto &i : lines) {
-        auto tmp = i 
-            | std::views::split(' ') 
-            | std::views::transform([](auto x) {
-                return std::stoi(std::string(x.begin(), x.end()));})
-            | tl::to<std::vector<int>>();
-        
-        if (checkSafety(tmp)) sum1++;
-        if (checkSafetyTwo(tmp)) sum2++;
-    } 
+  int ttl1 = 0, ttl2 = 0;
+  while (std::getline(file, line)) {
+    auto vec = line | std::views::split(' ') |
+               std::views::transform([](auto&& subrange) {
+                 int value = 0;
+                 std::string_view sv(subrange.begin(), subrange.end());
+                 std::from_chars(sv.data(), sv.data() + sv.size(), value);
+                 return value;
+               }) |
+               std::ranges::to<vi>();
+    if (checkSafety(vec)) ttl1++;
+    if (checkSafetyTwo(vec)) ttl2++;
+  }
 
-    printf("Part 1: %d\nPart 2: %d\n", sum1, sum2);
+  std::print("Part 1: {}\nPart 2: {}", ttl1, ttl2);
 }
 
 auto main() -> int {
-    solution();
+  solution("input/day2.txt");
 }
